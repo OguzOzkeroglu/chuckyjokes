fdedec236cb37dc71eb3aed800807f4130c0cbc8
Oguz Ozkeroglu <oguzozkeroglu@gmail.com>
2018-10-03 01:19:41 +0300

    initial commit

---
+++ src/main/java/com/ouz/chuckyjokes/ChuckNorrisJokesApplication.java
@@ -0,0 +1,12 @@
+package com.ouz.chuckyjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokesApplication.class, args);
+	}
+}
